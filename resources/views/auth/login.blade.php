88e909177c70dffbe582ebbad3e844bd40177f9c
alyaalawiyah <alyaaalawiyah00@gmail.com>
2025-06-11 15:11:44 +0700

    perbaiki file login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+@extends('layout/app')
+
+@section('title', 'Login Pengguna')
+
+@section('content')
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center">Login Pengguna</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" id="email" class="form-control is-invalid" value="{{ old('email') }}" required autofocus>
+                            @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" required>
+                            @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <button type="submit" class="btn btn-primary w-100">Login</button>
+                    </form>
+                </div>
+                <div class="card-footer text-center">
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
